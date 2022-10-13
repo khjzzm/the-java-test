7a8e3ed511021b521080caa98beba88b94ae3049
kimhyunjin <khjzzm@gmail.com>
2022-10-13 16:24:07 +0900

    JUnit 5 테스트 이름 표시하기

--- src/test/java/me/khjzzm/thejavatest/StudyTest.java
+++ src/test/java/me/khjzzm/thejavatest/StudyTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
-    void create() {
+    @DisplayName("스터디 만들기")
+    void create_new_study() {
         Study study = new Study();
         assertNotNull(study);
         System.out.println("create");
@@ -15,8 +18,8 @@ class StudyTest {
 
     @Test
     @Disabled
-    void create1(){
-        System.out.println("create1");
+    void create_new_study_again(){
+        System.out.println("create");
     }
 
     @BeforeAll
